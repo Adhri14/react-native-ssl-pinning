e00e6d80c58a56d4e52a5167d51a2842eea705b1
Adhri14 <adhri.adly@gmail.com>
2023-01-18 20:40:47 +0700

    testing

--- App.js
+++ App.js
@@ -5,52 +5,60 @@
  * @format
  * @flow strict-local
  */
-import React from 'react';
+import React, {useState} from 'react';
 
-import {
-  TouchableOpacity,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-
+import {TouchableOpacity, StyleSheet, Text, View} from 'react-native';
+import {fetch} from 'react-native-ssl-pinning';
 
 const App = () => {
+  const [result, setResult] = useState();
+  const fetchData = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+      method: 'GET',
+      timeoutInterval: 10000,
+      sslPinning: {
+        certs: [''],
+      },
+    })
+      .then(response => {
+        console.log(JSON.parse(response.bodyString));
+      })
+      .catch(err => {
+        console.log(`error: ${err}`);
+      });
+  };
 
-  const fetchData = () => { console.log("fetching data") }
-
-  const secureFetchData = () => { console.log("fetching secured data") }
+  const secureFetchData = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+      method: 'GET',
+      timeoutInterval: 10000,
+      sslPinning: {
+        certs: ['mycert'],
+      },
+    })
+      .then(response => {
+        const data = JSON.parse(response.bodyString);
+        console.log('data nya : ', data);
+        console.log('type data : ', typeof data.body);
+        setResult(data);
+      })
+      .catch(err => {
+        console.log(`error: ${err}`);
+      });
+  };
 
   return (
-    <View
-      contentInsetAdjustmentBehavior="automatic"
-      style={styles.container}
-    >
+    <View contentInsetAdjustmentBehavior="automatic" style={styles.container}>
+      <Text style={styles.header}>React Native SSL Pinning Testing</Text>
 
-      <Text style={styles.header}>
-        React Native SSL Pinning Testing
-      </Text>
-
-      <TouchableOpacity
-        style={styles.button}
-        onPress={fetchData}
-      >
-        <Text style={styles.text}>
-          Fetch data
-        </Text>
-
+      <TouchableOpacity style={styles.button} onPress={fetchData}>
+        <Text style={styles.text}>Fetch data</Text>
       </TouchableOpacity>
 
-      <TouchableOpacity
-        style={styles.button}
-        onPress={secureFetchData}
-      >
-        <Text style={styles.text}>
-          Fetch secured data
-        </Text>
-
+      <TouchableOpacity style={styles.button} onPress={secureFetchData}>
+        <Text style={styles.text}>Fetch secured data</Text>
       </TouchableOpacity>
-
+      <Text>{result?.id}</Text>
     </View>
   );
 };
@@ -59,21 +67,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
-
+    justifyContent: 'center',
   },
   header: {
     fontSize: 36,
     marginVertical: 10,
     marginBottom: 25,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   text: {
     fontSize: 24,
   },
   button: {
-    alignItems: "center",
-    backgroundColor: "blue",
+    alignItems: 'center',
+    backgroundColor: 'blue',
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 4,
